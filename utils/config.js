bec6851b52b2f54c5cd0b2411af209f3b997eb98
Gregory Bastianelli <greg@gregbastianelli.com>
2025-06-08 16:39:35 -0400

    express setup

--- utils/config.js
+++ utils/config.js
@@ -3,4 +3,4 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 
-modules.exports = { MONGODB_URI, PORT }
+module.exports = { MONGODB_URI, PORT }
