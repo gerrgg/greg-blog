70dc417f8f025ff836ae781cf177d71947063eb1
Greg Bastianelli <gregbast1994@gmail.com>
2025-06-09 06:26:27 -0400

    prevent logger from outputting during tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,6 +1,7 @@
 const { test, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const assert = require('node:assert')
 const app = require('../app')
 
 const api = supertest(app)
@@ -10,6 +11,19 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, 1)
+})
+
+test("a specific blog is within the returned blogs", async () => {
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+  assert(titles.includes('Node.js Wormholes and Event Horizons'));
 })
 
 after(async () => {
