23747c3d7f92032e150323cc7856fa6794e23b38
Gregory Bastianelli <greg@gregbastianelli.com>
2025-06-11 21:52:41 -0400

    integrate user route testing

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,29 +1,52 @@
-const Blog = require('../models/Blog')
+const Blog = require("../models/Blog");
+const User = require("../models/user");
 
 const initialBlogs = [
   {
-    title: 'HTML is easy',
-    likes: 10
+    title: "HTML is easy",
+    likes: 10,
   },
   {
-    title: 'Browser can execute only JavaScript',
-    likes: 20
-  }
-]
+    title: "Browser can execute only JavaScript",
+    likes: 20,
+  },
+];
+
+const initialUsers = [
+  {
+    username: "root",
+    name: "Superuser",
+    password: "password",
+  },
+  {
+    username: "user1",
+    name: "User One",
+    password: "password1",
+  },
+];
 
 const nonExistingId = async () => {
-  const blog = new Blog({ title: 'willremovethissoon' })
-  await blog.save()
-  await blog.deleteOne()
+  const blog = new Blog({ title: "willremovethissoon" });
+  await blog.save();
+  await blog.deleteOne();
 
-  return blog._id.toString()
-}
+  return blog._id.toString();
+};
 
 const blogsInDb = async () => {
-  const blogs = await Blog.find({})
-  return blogs.map(blog => blog.toJSON())
-}
+  const blogs = await Blog.find({});
+  return blogs.map((blog) => blog.toJSON());
+};
+
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((user) => user.toJSON());
+};
 
 module.exports = {
-  initialBlogs, nonExistingId, blogsInDb
-}
+  initialBlogs,
+  initialUsers,
+  nonExistingId,
+  blogsInDb,
+  usersInDb,
+};
---
+++ tests/user_api.test.js
@@ -0,0 +1,69 @@
+const assert = require("node:assert");
+const { test, describe, after, beforeEach } = require("node:test");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const helper = require("./test_helper");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+const api = supertest(app);
+
+describe("when there is initially one user in the db", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("password", 10);
+
+    const user = new User({
+      username: "testuser",
+      name: "Test User",
+      passwordHash,
+    });
+
+    await user.save();
+  });
+
+  test("creation succeeds with a valid username and password", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "newuser",
+      name: "New User",
+      password: "newpassword",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    assert(usernames.includes(newUser.username));
+  });
+
+  test("creation fails with a duplicate username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "testuser",
+      name: "Duplicate User",
+      password: "duplicatepassword",
+    };
+
+    const result = await api.post("/api/users").send(newUser).expect(400);
+
+    assert.strictEqual(result.body.error, "username must be unique");
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+  });
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
