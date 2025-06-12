be40ecd0dca9166ca35e7ce86d3a82698fd25010
Greg Bastianelli <gregbast1994@gmail.com>
2025-06-11 22:26:02 -0400

    connect blogs and users, populate

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,7 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 blogRouter.get('/', async (request, response, next) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -24,14 +24,25 @@ blogRouter.delete('/:id', async (request, response, next) => {
 blogRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
+  if( !user) {
+    return response.status(400).json({ error: 'userId is missing or invalid' })
+  }
+
   const blog = new Blog({
     title: body.title,
     author: body.author ?? '',
     url: body.url ?? '',
-    likes : body.likes ?? 0
+    likes : body.likes ?? 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
